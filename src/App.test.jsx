f56da01c8e0b63b4102a9092770a0e04f7ccd54e
ayc <ayc-vcs.m9xhr@aleeas.com>
2023-09-05 12:00:35 +0530

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,15 +5,16 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import renderer from 'react-test-renderer';
 import App from './App.jsx';
 
 describe('<App /> component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const root = ReactDOMClient.createRoot(div);
+    root.render(<App />);
+    root.unmount();
   });
 
   it('matches snapshot', () => {
